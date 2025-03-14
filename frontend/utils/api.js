5caf53ebd25c4f22daf5e652c2d63ef6f399aa45
Faiz Mansoor <1faizmansoor123@gmail.com>
2025-03-14 23:22:42 +0530

    env change

--- frontend/utils/api.js
+++ frontend/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:3000",
+  baseURL: import.meta.env.VITE_BACKEND_URL || "http://localhost:3000",
   withCredentials: true, // Ensures cookies are sent
 });
 
